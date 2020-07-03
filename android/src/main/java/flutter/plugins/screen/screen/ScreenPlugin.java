a8a416f28419102eeff47b9635c88885d9711309
WangKing027 <1970562876@qq.com>
2020-07-03 16:04:09 +0800

    Update ScreenPlugin.java
    兼容 v2

--- android/src/main/java/flutter/plugins/screen/screen/ScreenPlugin.java
+++ android/src/main/java/flutter/plugins/screen/screen/ScreenPlugin.java
@@ -1,8 +1,13 @@
 package flutter.plugins.screen.screen;
 
+import android.app.Activity;
 import android.provider.Settings;
 import android.view.WindowManager;
 
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
@@ -12,65 +17,84 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 /**
  * ScreenPlugin
  */
-public class ScreenPlugin implements MethodCallHandler {
+public class ScreenPlugin implements FlutterPlugin, MethodCallHandler {
 
-  private ScreenPlugin(Registrar registrar){
-    this._registrar = registrar;
-  }
-  private Registrar _registrar;
+    private Activity activity ;
+    private MethodChannel methodChannel ;
 
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "github.com/clovisnicolas/flutter_screen");
-    channel.setMethodCallHandler(new ScreenPlugin(registrar));
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    switch(call.method){
-      case "brightness":
-        result.success(getBrightness());
-        break;
-      case "setBrightness":
-        double brightness = call.argument("brightness");
-        WindowManager.LayoutParams layoutParams = _registrar.activity().getWindow().getAttributes();
-        layoutParams.screenBrightness = (float)brightness;
-        _registrar.activity().getWindow().setAttributes(layoutParams);
-        result.success(null);
-        break;
-      case "isKeptOn":
-        int flags = _registrar.activity().getWindow().getAttributes().flags;
-        result.success((flags & WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) != 0) ;
-        break;
-      case "keepOn":
-        Boolean on = call.argument("on");
-        if (on) {
-          System.out.println("Keeping screen on ");
-          _registrar.activity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        }
-        else{
-          System.out.println("Not keeping screen on");
-          _registrar.activity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        }
-        result.success(null);
-        break;
-
-      default:
-        result.notImplemented();
-        break;
+    private ScreenPlugin(Activity activity){
+        this.activity = activity;
     }
-  }
 
-  private float getBrightness(){
-    float result = _registrar.activity().getWindow().getAttributes().screenBrightness;
-    if (result < 0) { // the application is using the system brightness
-      try {
-        result = Settings.System.getInt(_registrar.context().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS) / (float)255;
-      } catch (Settings.SettingNotFoundException e) {
-        result = 1.0f;
-        e.printStackTrace();
-      }
+    public static void registerWith(Registrar registrar) {
+        final ScreenPlugin instance = new ScreenPlugin(registrar.activity());
+        instance.onAttachedToEngine(registrar.messenger());
     }
-    return result;
-  }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        switch(call.method){
+            case "brightness":
+                result.success(getBrightness());
+                break;
+            case "setBrightness":
+                double brightness = call.argument("brightness");
+                WindowManager.LayoutParams layoutParams = activity.getWindow().getAttributes();
+                layoutParams.screenBrightness = (float)brightness;
+                activity.getWindow().setAttributes(layoutParams);
+                result.success(null);
+                break;
+            case "isKeptOn":
+                int flags = activity.getWindow().getAttributes().flags;
+                result.success((flags & WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) != 0) ;
+                break;
+            case "keepOn":
+                Boolean on = call.argument("on");
+                if (on) {
+                    System.out.println("Keeping screen on ");
+                    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                }
+                else{
+                    System.out.println("Not keeping screen on");
+                    activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                }
+                result.success(null);
+                break;
+
+            default:
+                result.notImplemented();
+                break;
+        }
+    }
+
+    private float getBrightness(){
+        float result = activity.getWindow().getAttributes().screenBrightness;
+        if (result < 0) { // the application is using the system brightness
+            try {
+                result = Settings.System.getInt(activity.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS) / (float)255;
+            } catch (Settings.SettingNotFoundException e) {
+                result = 1.0f;
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    private void onAttachedToEngine(BinaryMessenger messenger){
+        methodChannel = new MethodChannel(messenger, "github.com/clovisnicolas/flutter_screen");
+        methodChannel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        onAttachedToEngine(binding.getBinaryMessenger());
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        methodChannel.setMethodCallHandler(null);
+        methodChannel = null ;
+        activity = null ;
+    }
 
 }
